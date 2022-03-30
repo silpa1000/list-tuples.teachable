dab78fe7a4b50f2089cdcc52e39c2f3f73a9b130
silpa1000 <100128518+silpa1000@users.noreply.github.com>
2022-03-30 10:44:33 +0530

    Add files via upload

---
+++ printodd.py
@@ -0,0 +1,10 @@
+# Python program to print odd numbers
+
+# declare list
+my_list = [1,2,3,4,5,6,7,8,9]
+
+# for loop
+for num in my_list:
+    # checking condition
+    if num % 2 != 0:
+        print(num)
