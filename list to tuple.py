cedfe0144bff4b3f56777e7dfc8e072b7e107e93
silpa1000 <100128518+silpa1000@users.noreply.github.com>
2022-03-30 10:39:33 +0530

    Add files via upload

---
+++ list to tuple.py
@@ -0,0 +1,10 @@
+#Write a python program to convert a list into a tuple.
+
+list1 = ['Saranya', 'Surya', 'Manu']
+
+#convert list into tuple
+tuple1 = tuple(list1)
+
+print(tuple1)
+print(type(tuple1))
+ 
